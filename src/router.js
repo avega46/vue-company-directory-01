0c7586474c26822b9302ecdb266cb8abf835d72c
Alan Vega <avega46@stu.southtexascollege.edu>
2023-02-19 02:37:31 -0600

    added router

--- src/router.js
+++ src/router.js
@@ -1,15 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomePage from '@/views/HomePage.vue'
+import MainPage from '@/components/MainPage.vue'
+import LoginPage from '@/components/LoginPage.vue'
+import NotFound from '@/components/NotFound.vue'
+
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/', name: 'Home', component: MainPage },
+  { path: '/login', name : 'Login', component: LoginPage },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound}
 ]
 
-const router = createRouter({
+export default createRouter({
   history: createWebHistory(),
   routes,
 })
+///import HomePage from '@/views/HomePage.vue'
 
-export default router
+///const routes = [
+///  { path: '/', name: 'Home', component: HomePage },
+///  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+///]
+
+///const router = createRouter({
+///  history: createWebHistory(),
+///  routes,
+///})
+
+///export default router
